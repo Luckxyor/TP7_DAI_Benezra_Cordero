19a82a43f32a4e89e2b740680b0a5627be3d2efe
LUCAS EIAL BENEZRA <48313633@ort>
2025-05-09 10:37:44 -0300

    Empezamos

--- App.js
+++ App.js
@@ -1,20 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, View, ImageBackground, Text } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
       <StatusBar style="auto" />
-    </View>
+      <View style={styles.imageContainer}>
+        <ImageBackground 
+          source={{uri:'https://i.ytimg.com/vi/3h65dBk-f0s/maxresdefault.jpg'}}
+          resizeMode='cover'
+          style={styles.image}
+        />
+      </View>
+      <Text>MARASI EL CARNICERO DEL CODIGO</Text>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent:'center',
+    alignItems:'center',
   },
+  imageContainer: {
+    width: 200,
+    height: 200,
+    borderRadius: 100,
+    overflow: 'hidden',
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+  },
+  Titulo:{
+    fontSize: 20,
+
+  }
 });
